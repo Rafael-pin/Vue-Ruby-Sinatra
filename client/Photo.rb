637b545bf14bd811a193c00d1bd366c4b8647061
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-11 18:03:09 -0300

    Photo class add

---
+++ client/Photo.rb
@@ -0,0 +1,14 @@
+class Photo
+  attr_accessor :idPhoto
+  attr_accessor :photo
+  attr_accessor :description
+  
+    def initialize(idPhoto, photo, description)
+      @idPhoto = idPhoto
+      @photo = photo
+      @description = description
+    end
+
+    
+  end
+end
