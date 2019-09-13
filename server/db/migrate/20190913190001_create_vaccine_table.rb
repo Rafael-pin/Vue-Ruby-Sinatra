2468260c20283652b4abfbaeade71e5960c2233b
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-13 16:22:56 -0300

    vaccine table

--- server/db/migrate/20190913190001_create_vaccine_table.rb
+++ server/db/migrate/20190913190001_create_vaccine_table.rb
@@ -1,4 +1,10 @@
 class CreateVaccineTable < ActiveRecord::Migration[6.0]
   def change
+    create_table :vaccine do |t|
+      t.int :id_vaccine
+      t.string :name
+      t.time :date
+      t.int :duration
+    end
   end
 end
