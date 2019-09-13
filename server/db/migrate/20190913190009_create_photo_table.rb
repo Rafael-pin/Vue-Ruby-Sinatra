af179f798a5675378440dd9f3f4d8850e9340207
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-13 16:23:10 -0300

    photo table

--- server/db/migrate/20190913190009_create_photo_table.rb
+++ server/db/migrate/20190913190009_create_photo_table.rb
@@ -1,4 +1,8 @@
 class CreatePhotoTable < ActiveRecord::Migration[6.0]
   def change
+    create_table :photo do |t|
+      t.int :id_photo
+      t.string :photo
+      t.string :description
   end
 end
