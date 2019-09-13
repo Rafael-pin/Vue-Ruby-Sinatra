fea244d10987a070576d656408e00fda3f181a90
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-13 16:46:25 -0300

    fixed time to timestamp and photo to string

--- server/db/migrate/20190913185825_create_pet_table.rb
+++ server/db/migrate/20190913185825_create_pet_table.rb
@@ -7,12 +7,10 @@ class CreatePerTable < ActiveRecord::Migration[5.0]
     create_table :pet do |t|
       t.int :id_pet
       t.string :name
-      t.time :birth_date
+      t.timestamp :birth_date
       t.double :weight
-      t.Photo :profile_photo
+      t.string :profile_photo
       t.string :type
-      t.Photo :photos
-      t.Vaccine :vaccines
     end
   end
 end
