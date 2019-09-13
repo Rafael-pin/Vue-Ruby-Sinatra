6c52322644d8a7c96b8f1bbed49f2487e427b441
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-13 16:46:34 -0300

    time to timestamp

--- server/db/migrate/20190913190001_create_vaccine_table.rb
+++ server/db/migrate/20190913190001_create_vaccine_table.rb
@@ -3,7 +3,7 @@ class CreateVaccineTable < ActiveRecord::Migration[6.0]
     create_table :vaccine do |t|
       t.int :id_vaccine
       t.string :name
-      t.time :date
+      t.timestamp :date
       t.int :duration
     end
   end
