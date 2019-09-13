d3f849c4f291e2cca725383e2b1a0fa9dcf96093
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-13 14:12:39 -0300

    Variables are now in one line

--- server/photo.rb
+++ server/photo.rb
@@ -1,7 +1,5 @@
 class Photo
-  attr_accessor :id_photo
-  attr_accessor :photo
-  attr_accessor :description
+  attr_accessor :id_photo, :photo, :description
   
     def initialize(id_photo, photo, description)
       @id_photo = id_photo
