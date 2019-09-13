df53b8dc7b8878979c62f7b001fc74ff2391d4c7
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-13 14:12:20 -0300

    Some methods are different from before.

--- server/vaccine.rb
+++ server/vaccine.rb
@@ -1,8 +1,5 @@
 class Vaccine
-  attr_accessor :idVaccine
-  attr_accessor :name
-  attr_accessor :date
-  attr_accessor :duration
+  attr_accessor :idVaccine, :name, :date, :duration
 
   def initialize(id_vaccine, name, date, duration)
     @id_vaccine = idVaccine
@@ -11,28 +8,23 @@ class Vaccine
     @duration = duration
   end
 
-
-
   def calculate_duration_date
-    return @date + @duration * 86400
+    @date + @duration * 86400
   end
 
   def show_results
     expired_date = calculate_duration_date.strftime("%d/%m/%Y")
-    if (calculate_duration_date <= Time.now.utc)
 
-      return "This vaccine has expired in #{expired_date}" 
+    return "This vaccine has expired in #{expired_date}" if (calculate_duration_date <= Time.now.utc)
 
-    end
-
-    return "this vaccine will expire in #{expired_date}" +
-      " you have #{(calculate_duration_date - Time.now.to_i).strftime("%d")} day(s) to vaccine your pet again"
+    "this vaccine will expire in #{expired_date}
+    you have #{(calculate_duration_date - Time.now.to_i).strftime("%d")} day(s) to vaccine your pet again"
+    
   end
 
   vaccine = Vaccine.new(1,"raiva",Time.new(2019, 9, 12),10)
 
   puts vaccine.name
   puts vaccine.show_results
-
 
 end
