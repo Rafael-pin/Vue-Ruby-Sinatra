690b07e01b7f68dceb279ad2cf92ec922924be75
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-17 12:33:39 -0300

    types done

---
+++ server/graphql/types/photo-types.rb
@@ -0,0 +1,12 @@
+require 'graphql'
+require_relative 'base_object'
+
+class PhotoTypes::Photo < Types::BaseObject
+  description 'Resembles a photo Object Type'
+
+  field :id, ID, null: false
+  # field :name, String, null: false
+  # field :twitter_handle, String, null: true
+  # field :bio, String, null: true
+  # field :talk_title, String, null: true
+end
---
+++ server/graphql/types/vaccine_types.rb
@@ -0,0 +1,12 @@
+require 'graphql'
+require_relative 'base_object'
+
+class VaccineTypes::Vaccine < Types::BaseObject
+  description 'Resembles a vaccine Object Type'
+
+  field :id, ID, null: false
+  # field :name, String, null: false
+  # field :twitter_handle, String, null: true
+  # field :bio, String, null: true
+  # field :talk_title, String, null: true
+end
