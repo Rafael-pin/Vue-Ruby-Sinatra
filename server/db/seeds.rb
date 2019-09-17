1a125c1ebed2899fe1d0ac0825d1c43948ee2cbd
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-17 16:01:38 -0300

    a few things are now commented

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,9 +1,9 @@
 pets = {id_pet: 01, name: 'Jon', birth_date: '2018-09-10', weight: 12, profile_photo: 'wdwdwd', kind: 'dog'}
 
-vaccines = {id_vaccine: 01, name: "raiva", date: "2018-09-16", duration: 15}
+# vaccines = {id_vaccine: 01, name: "raiva", date: "2018-09-16", duration: 15}
 
-photos = {id_photo: 01, photo: "https//exemplo", description: "cachorro"}
+# photos = {id_photo: 01, photo: "https//exemplo", description: "cachorro"}
 
 Pet.create(pets)
-Vaccine.create(vaccines)
-Photo.create(photos)
+# Vaccine.create(vaccines)
+# Photo.create(photos)
