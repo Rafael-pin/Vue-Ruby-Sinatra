221bee3298c99d4e283f7ccd68de9928a267a751
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-18 17:35:57 -0300

    add MutationType

--- server/graphql/schema.rb
+++ server/graphql/schema.rb
@@ -1,6 +1,8 @@
 require 'graphql'
-require_relative 'query'
+require_relative './query'
+require_relative './types/mutation_type'
 
 class ConferenceAppSchema < GraphQL::Schema
   query QueryType
+  mutation MutationType
 end
