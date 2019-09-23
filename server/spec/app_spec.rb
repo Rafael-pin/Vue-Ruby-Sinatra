99eb8735626ed4e2c8b08f2f36390dc5427a7c68
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-23 15:01:00 -0300

    new tests

--- server/spec/app_spec.rb
+++ server/spec/app_spec.rb
@@ -40,7 +40,50 @@ describe "Pet.age" do
 
 end
 
-  # it "expect to recive an age = 10" do
-  #   pet = Pet.new(birth_date: "2009-09-17")
-  #   expect(pet.age).to eq(9)
-  # end
+describe "Pet.life_stage" do
+  before do
+    time_now = Time.local(2019, 9, 23)
+    Timecop.travel(time_now)
+    Timecop.freeze(Time.now)
+   end
+
+  after do
+    Timecop.return
+  end
+
+  it "expect to return puppy" do
+    pet = Pet.new(birth_date: "20019-09-22")
+    expect(pet.life_stage).to eq("puppy")
+  end
+
+  it "expect to return adult" do
+    pet = Pet.new(birth_date: "2017-09-23")
+    expect(pet.life_stage).to eq("adult")
+  end
+  it "expect to return old" do
+    pet = Pet.new(birth_date: "2011-09-23")
+    expect(pet.life_stage).to eq("old")
+  end
+end
+
+describe "Vaccine.show_results" do
+  before do
+    time_now = Time.local(2019, 9, 23)
+    Timecop.travel(time_now)
+    Timecop.freeze(Time.now)
+   end
+
+  after do
+    Timecop.return
+  end
+
+  it "expect to return expire date and days to vaccine" do
+    vaccine = Vaccine.new(date: "2019-09-23", duration: "30")
+    expect(vaccine.show_results).to eq("this vaccine will expire in 23/10/2019 /\n    you have 30 day(s) to vaccine your pet again")
+  end
+
+  it "expect to return expire date and days to vaccine" do
+    vaccine = Vaccine.new(date: "2019-09-01", duration: "9")
+    expect(vaccine.show_results).to eq("This vaccine has expired in 10/09/2019")
+  end
+  end
