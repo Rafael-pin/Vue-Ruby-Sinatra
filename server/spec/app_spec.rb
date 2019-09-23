32f65ec8316c92382a124e6f02c75ea8b305c5ca
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-23 18:35:55 -0300

    new tests

--- server/spec/app_spec.rb
+++ server/spec/app_spec.rb
@@ -86,4 +86,48 @@ describe "Vaccine.show_results" do
     vaccine = Vaccine.new(date: "2019-09-01", duration: "9")
     expect(vaccine.show_results).to eq("This vaccine has expired in 10/09/2019")
   end
+end
+
+describe "Vaccine.calculate_duration_date" do 
+  before do
+    time_now = Time.local(2019, 9, 23)
+    Timecop.travel(time_now)
+    Timecop.freeze(Time.now)
   end
+
+  after do
+    Timecop.return
+  end
+
+  it "expect to return the date of expiration 01/10/2019" do
+    vaccine = Vaccine.new(date: "2019-09-01", duration: "30")
+    expect(vaccine.calculate_duration_date.strftime("%d/%m/%Y")).to eq("01/10/2019")
+  end
+
+  it "expect to return the date of expiration 01/12/2019" do
+    vaccine = Vaccine.new(date: "2019-09-01", duration: "94")
+    expect(vaccine.calculate_duration_date.strftime("%d/%m/%Y")).to eq("04/12/2019")
+  end
+end
+
+describe "Expect to return a boolean" do
+  before do
+    time_now = Time.local(2019, 9, 23)
+    Timecop.travel(time_now)
+    Timecop.freeze(Time.now)
+  end
+
+  after do
+    Timecop.return
+  end
+
+  it "expect to return true" do
+    pet = Pet.new(birth_date: "2018-09-23")
+    expect(pet.made_birthday_this_year?).to eq(true)
+  end
+
+  it "expect to return false" do
+    pet = Pet.new(birth_date: "2018-09-22")
+    expect(pet.made_birthday_this_year?).to eq(false)
+  end
+end
