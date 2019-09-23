c4b3cf635d6543b2016f6265a0303afc67a06585
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-23 15:00:36 -0300

    estetic changes

--- server/models/vaccine.rb
+++ server/models/vaccine.rb
@@ -2,17 +2,19 @@ class Vaccine < ActiveRecord::Base
   belongs_to :pet
 
   def calculate_duration_date
-    @date + @duration * 86400
+    date + duration * 86400
   end
 
   def show_results
     expired_date = calculate_duration_date.strftime("%d/%m/%Y")
 
-    return "This vaccine has expired in #{expired_date}" if (calculate_duration_date <= Time.now.utc)
+    if (calculate_duration_date <= Time.now.utc)
 
-    "this vaccine will expire in #{expired_date}
-    you have #{(calculate_duration_date - Time.now.to_i).strftime("%d")} day(s) to vaccine your pet again"
-    
+    return "This vaccine has expired in #{expired_date}"
+
+    end
+
+    "this vaccine will expire in #{expired_date} /
+    you have #{(calculate_duration_date - Time.now.to_i).strftime("%d")} day(s) to vaccine your pet again"  
   end
-
 end
