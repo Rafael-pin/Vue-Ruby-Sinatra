ea1686db1dfd7bd893a9238ee16f7a0090b52145
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-25 18:07:22 -0300

    importing components

---
+++ client/vue-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <link rel="stylesheet" href="https://unpkg.com/primitive-ui/dist/css/main.css" />
+    <page-header/>
+    <page-content/>
+    <vaccine-grid/>
+  </div>
+</template>
+
+<script>
+import PageHeader from './components/page-header.vue'
+import PageContent from './components/page-content.vue'
+import VaccineGrid from './components/vaccine-grid.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PageHeader,
+    PageContent,
+    VaccineGrid
+  }
+}
+</script>
+
+<style scoped>
+  #app {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+</style>
