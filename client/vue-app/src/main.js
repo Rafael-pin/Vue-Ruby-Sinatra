e7bb0272a5a05827987af025a15f11145a3bb609
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-30 13:10:12 -0300

    Using Apollo now

--- client/vue-app/src/main.js
+++ client/vue-app/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApollo from 'vue-apollo'
+import apollo from './apolloClient'
 
 Vue.config.productionTip = false
+Vue.use(VueApollo)
+Vue.prototype.$apollo = apollo
+
+const apolloProvider = new VueApollo({ defaultClient: apollo})
 
 new Vue({
   render: h => h(App),
+  apolloProvider
 }).$mount('#app')
