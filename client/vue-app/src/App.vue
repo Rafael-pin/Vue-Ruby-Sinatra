0991080644d227f755428a8364dcfe146bb953af
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-09-30 13:09:45 -0300

    a few thing are different now

--- client/vue-app/src/App.vue
+++ client/vue-app/src/App.vue
@@ -5,24 +5,42 @@
     <page-header/>
     <page-content/>
     <vaccine-grid/>
+    {{ pets }}
   </div>
 </template>
 
 <script>
+import gql   from 'graphql-tag'
 import PageHeader from './components/page-header.vue'
 import PageContent from './components/page-content.vue'
 import VaccineGrid from './components/vaccine-grid.vue'
 
+
 export default {
   name: 'app',
+  // async created() {
+  //   debugger;
+  //   const response = await this.$apollo.query({
+  //     query: gql`{ pets { name } }`
+  //   });
+  //   debugger;
+  //   // eslint-disable-next-line no-console
+  //   console.log(response.data.pets);
+  // },
+  apollo:{
+    pets: {
+      query: gql`query pets{ pets { name } }`
+    }
+  },
   components: {
     PageHeader,
     PageContent,
-    VaccineGrid
+    VaccineGrid,
+    // PetProfile
   },
   data() {
     return {
-      //make return from ... GraphQL?
+      pets: null
     }
   },
 }
