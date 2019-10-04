fa229e18a42b545258623c793f3511fd7f78bd54
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-10-04 13:41:14 -0300

    rename in id

--- server/graphql/types/photo_type.rb
+++ server/graphql/types/photo_type.rb
@@ -4,7 +4,7 @@ require_relative 'base_object'
 class Types::PhotoType < Types::BaseObject
   description 'Resembles a photo Object Type'
 
-  field :id_photo, ID, null: false
+  field :id, ID, null: false
   field :photo, String, null: false
   field :description, String, null: true
   
--- server/graphql/types/vaccine_type.rb
+++ server/graphql/types/vaccine_type.rb
@@ -4,9 +4,11 @@ require_relative 'base_object'
 class Types::VaccineType < Types::BaseObject
   description 'Resembles a vaccine Object Type'
 
-  field :id_vaccine, ID, null: false
-  field :name, String, null: false
+  field :id, ID, null: false
+  field :name, String, null: true
   field :date, String, null: true
   field :duration, Integer, null: true
+  field :show_results, String, null: true
+  field :calculate_duration_date, Integer, null: true
 
 end
