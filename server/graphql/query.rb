f71468e354700350a2e086c8cae0175551c7ab90
Rafael Pinheiro <rafael.pinheiro.rafa@icloud.com>
2019-10-04 13:40:41 -0300

    new query created

--- server/graphql/query.rb
+++ server/graphql/query.rb
@@ -10,20 +10,30 @@ class QueryType < GraphQL::Schema::Object
     description 'Get all pets of the system'
   end
 
-  field :vaccines, [Types::VaccineType], null: false do
+  field :vaccines, [Types::VaccineType], null: true do
     description 'Get all vaccines of the system'
+    argument :id , ID, required: true
   end
 
-  field :photos, [Types::PhotoType], null: false do
+  field :photos, [Types::PhotoType], null: true do
     description 'Get all photos of the system'
+  end
+
+  field :pet, Types::PetType, null: true do
+    description 'Get a pet of the system'
+    argument :id , ID, required: true
   end
 
   def pets
     Pet.all
   end
 
-  def vaccines
-    Vaccine.all
+  def pet(id)
+    Pet.find_by(id)
+  end
+
+  def vaccines(id)
+    Vaccine.find_by(pet_id: Pet.id) if id.present?
   end
 
   def photos
